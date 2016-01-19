171b4683d1662d567ca3ed0f6fd8b19a03cf65d6
Carlie Copeland <carliecope@gmail.com>
2016-01-18 18:51:02 -0600

    add build

--- js/components/graphics/bird.js
+++ js/components/graphics/bird.js
@@ -2,8 +2,11 @@ var BirdGraphicsComponent = function(entity) {
     this.entity = entity;
 };
 
-BirdGraphicsComponent.prototype.draw = function() {
+BirdGraphicsComponent.prototype.draw = function(context) {
     console.log("Drawing a bird");
+    context.beginPath();
+    context.arc(50, 50, 10, 0, 2 * Math.PI);
+    context.fill();
 };
 
 exports.BirdGraphicsComponent = BirdGraphicsComponent;
